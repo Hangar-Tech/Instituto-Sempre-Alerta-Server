b31894c2248c850689acf7e6ffaf0f9a143ffaee
MatheusVict <matheusvictorhenrique@gmail.com>
2024-02-19 21:53:54 -0300

    feat: initial commit
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/main/java/com/institutosemprealerta/semprealerta/SempreAlertaApplication.java
@@ -0,0 +1,13 @@
+package com.institutosemprealerta.semprealerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SempreAlertaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SempreAlertaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/institutosemprealerta/semprealerta/SempreAlertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institutosemprealerta.semprealerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SempreAlertaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
