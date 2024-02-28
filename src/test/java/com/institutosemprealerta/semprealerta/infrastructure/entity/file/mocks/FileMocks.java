0550cfdde53607951e7c5b51bd906cded04091c4
MatheusVict <matheusvictorhenrique@gmail.com>
2024-02-28 12:45:53 -0300

    test: jpa file repository adapter
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/test/java/com/institutosemprealerta/semprealerta/infrastructure/entity/file/mocks/FileMocks.java
+++ src/test/java/com/institutosemprealerta/semprealerta/infrastructure/entity/file/mocks/FileMocks.java
@@ -1,9 +1,18 @@
 package com.institutosemprealerta.semprealerta.infrastructure.entity.file.mocks;
 
+import com.institutosemprealerta.semprealerta.domain.model.File;
 import com.institutosemprealerta.semprealerta.infrastructure.entity.file.FileEntity;
 
 public class FileMocks {
     public static FileEntity createFileEntity() {
         return new FileEntity("fileName", "fileDownloadUri", "fileType");
+    }
+
+    public static File createFile() {
+        return File.builder()
+                .fileName("fileName")
+                .fileDownloadUri("fileDownloadUri")
+                .fileType("fileType")
+                .build();
     }
 }
